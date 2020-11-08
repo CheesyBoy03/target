ec89a5cb6e549e13cf0d28261951046c528e4f9e
Van Step <van.step2186@gmail.com>
2020-11-08 12:05:33 +0400

    first commit

---
+++ js/popup.js
@@ -0,0 +1,13 @@
+let popup = document.querySelector('.popup');
+
+function viewPopup() {
+    popup.classList.add('open');
+};
+
+function closePopup() {
+    popup.classList.remove('open');
+};
+
+function dispatch() {
+    
+}
